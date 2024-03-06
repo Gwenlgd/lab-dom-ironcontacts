03d63c8ce64cf7bb47c766e2e69151f6e80e3083
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-03-06 15:30:55 +0100

    Add 3 contacts and delete button is working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 // HTML ELEMENTS
+
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
-const randomIndex = Math.floor(Math.random() * contacts.length);
-const splicedArr = contacts.splice(randomIndex, 1);
+// const randomIndex = Math.floor(Math.random() * contacts.length);
+// const splicedArr = contacts.splice(randomIndex, 1);
 
 // Get the element from the spliced array
-const randomContact = splicedArr[0];
+// const randomContact = splicedArr[0];
 
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
+// const exampleRow = document.createElement("tr");
+// exampleRow.innerHTML = `
+//   <td>
+//     <img src="${randomContact.pictureUrl}" />
+//   </td>
+//   <td> ${randomContact.name} </td>
+//   <td> ${randomContact.popularity.toFixed(2)} </td>
+//   <td>
+//     <button class="btn-delete">Delete</button>
+//   </td>
+//   <td>
+//     <button class="btn-like">
+//       <img src="./images/icon.png" alt="like" />
+//     </button>
+//   </td>
+// `;
+
+// tableBody.appendChild(exampleRow);
+
+
+// ITERATION 1 - Display 3 contacts
+// Get the first 3 contacts from the 'contacts' array.
+const threeContacts = contacts.splice(0, 3);
+
+// Your code goes here ...
+function getContact() {
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const splicedArr = contacts.splice(randomIndex, 1);
+  const randomContact = splicedArr[0];
+
+  const createNewRow = document.createElement("tr");
+  createNewRow.innerHTML = `
   <td>
     <img src="${randomContact.pictureUrl}" />
   </td>
@@ -28,32 +58,31 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
+  tableBody.appendChild(createNewRow);
+}
+
+threeContacts.forEach((contact) => {
+  getContact(contact);
+})
+
+// ITERATION 2 - Delete Buttons
+
+// Your code goes here ...
+
+tableBody.addEventListener("click", (event) => {
+  if (event.target.classList.contains("btn-delete")) {
+    const row = event.target.closest("tr");
+    row.remove();
+  }
+});
 
 
-
-
-
-// ITERATION 1 - Display 3 contacts
-// Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
+// ITERATION 3 - Like Buttons
 
 // Your code goes here ...
 
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
 
-  // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
